8de09c17e089ea919f5743277cabea7b4d210552
Thalles Lorrhan <thalles.lorrhan30@gmail.com>
2023-07-20 16:04:21 -0300

    add settings

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+bind = '0.0.0.0:8000'
+workers = 2
